a7e0cabec7a8dea5e6dc7b6d9828edfd688c89c0
Vijayendher Gatla <vijayendhergatla@Vijayendhers-MacBook-Air.local>
2024-07-09 11:13:38 +0530

    Initial Commit

---
+++ src/routes/blog/list/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Table from "./Table.svelte";
+</script>
+<h1>List of blog</h1>
+<Table/>
---
+++ src/routes/blog/list/Table.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Navbar from "$lib/Navbar.svelte";
+</script>
+<h1>This is my table</h1>
+<Navbar/>
