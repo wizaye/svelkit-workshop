255c105cdab46ee54269cff78fe0773e7bb24fc4
Vijayendher Gatla <vijayendhergatla@Vijayendhers-MacBook-Air.local>
2024-07-17 10:42:55 +0530

    added postgres

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,4 +1,9 @@
-<script>
+<script lang="ts">
     export let data;
 </script>
-<p class="font-bold text-center">{data.content}</p>
+
+<div>
+  {#each data.names as { name } }
+    <div class="font-semibold">{name}}</div>
+  {/each}
+</div>
